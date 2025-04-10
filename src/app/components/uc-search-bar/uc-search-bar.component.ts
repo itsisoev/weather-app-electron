c316d87f5bc237b9609c35f25d54b6c02b32165d
Daler <isoev9733@gmail.com>
2025-04-10 23:11:19 +0400

    Доработка отображений погоды

--- src/app/components/uc-search-bar/uc-search-bar.component.ts
+++ src/app/components/uc-search-bar/uc-search-bar.component.ts
@@ -1,13 +1,34 @@
-import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {debounceTime, Subject} from "rxjs";
 
 @Component({
   selector: 'uc-search-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './uc-search-bar.component.html',
   styleUrl: './uc-search-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UcSearchBarComponent {
   placeholder = input<string>("")
+  searchChange = output<string>();
+
+  search = "";
+
+  private searchSubject = new Subject<string>();
+
+  constructor() {
+    this.searchSubject.pipe(
+      debounceTime(500)
+    ).subscribe(value => {
+      this.searchChange.emit(value);
+    });
+  }
+
+  onSearchChange() {
+    this.searchSubject.next(this.search);
+  }
 }
---
+++ src/app/components/uc-sun/uc-sun.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'uc-sun',
+  standalone: true,
+  imports: [],
+  templateUrl: './uc-sun.component.html',
+  styleUrl: './uc-sun.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UcSunComponent {
+
+}
