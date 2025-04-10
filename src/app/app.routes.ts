7f69927030e58d0670f3b521da6a60ab4d8767a3
Daler <isoev9733@gmail.com>
2025-04-10 19:01:55 +0400

    внес мелкие правки

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
-import {CardWeatherComponent} from "./shared/components/card-weather/card-weather.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    children: [
-      {
-        path: '',
-        component: CardWeatherComponent
-      }
-    ]
+    children: []
   }
 ];
