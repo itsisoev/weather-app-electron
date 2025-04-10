dfda2db82544aae3bb457e3c8614f2a4b2ddf01d
Daler <isoev9733@gmail.com>
2025-04-10 16:18:14 +0400

    Доработка компонентов

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,16 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {UcHeaderComponent} from "../../components/uc-header/uc-header.component";
+import {ForecastComponent} from "../../features/forecast/forecast.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [
-        UcHeaderComponent
-    ],
+  imports: [
+    UcHeaderComponent,
+    ForecastComponent,
+    RouterOutlet
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
